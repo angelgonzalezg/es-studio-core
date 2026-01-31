b0c19d996b14433d9e5f507f22827361660c99d1
Angel Gonzalez <angel.glez.g@gmail.com>
2026-01-30 20:24:35 -0600

    🎉 tada: Initial commit for ES studio core business logic.

---
+++ src/main/java/com/esstudio/platform/esstudiocore/EsStudioCoreApplication.java
@@ -0,0 +1,13 @@
+package com.esstudio.platform.esstudiocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsStudioCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsStudioCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esstudio/platform/esstudiocore/EsStudioCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esstudio.platform.esstudiocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsStudioCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
